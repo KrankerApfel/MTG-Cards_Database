b177b616fc7b325b2415f068c66c4ab1e84092e9
sonny <sonny.ra@hotmail.fr>
2018-11-27 21:02:50 +0100

    update

--- Etape_3/création de table/magicCreateTable.mysql.sql
+++ Etape_3/création de table/magicCreateTable.mysql.sql
@@ -1,14 +1,13 @@
-DROP TABLE IF EXISTS serie CASCADE;
-DROP TABLE IF EXISTS langue CASCADE;
-DROP TABLE IF EXISTS langue_serie CASCADE;
-DROP TABLE IF EXISTS collection CASCADE;
+DROP TABLE IF EXISTS possession CASCADE;
+DROP TABLE IF EXISTS carte_langue CASCADE;
 DROP TABLE IF EXISTS carte_virtuelle CASCADE;
-DROP TABLE IF EXISTS carte_langue;
-DROP TABLE IF EXISTS possession;
-
+DROP TABLE IF EXISTS collection CASCADE;
+DROP TABLE IF EXISTS langue_serie CASCADE;
+DROP TABLE IF EXISTS langue CASCADE;
+DROP TABLE IF EXISTS serie CASCADE;
 
 CREATE TABLE serie (
- ser_code    INT AUTO_INCREMENT PRIMARY KEY,
+ ser_code    VARCHAR(3) PRIMARY KEY,
  ser_nom     VARCHAR(30) NOT NULL,
  ser_date    DATE,
  ser_nbTotal INTEGER NOT NULL
@@ -20,7 +19,7 @@ CREATE TABLE langue (
 
 CREATE TABLE langue_serie (
   lang_id  INT NOT NULL ,
-  ser_code INT NOT NULL ,
+  ser_code VARCHAR(3) NOT NULL ,
   FOREIGN KEY (lang_id) REFERENCES langue (lang_id),
   FOREIGN KEY (ser_code) REFERENCES serie (ser_code),
   PRIMARY KEY (lang_id, ser_code)
@@ -33,21 +32,21 @@ CREATE TABLE collection(
 
 CREATE TABLE carte_virtuelle (
  carte_id INT AUTO_INCREMENT PRIMARY KEY,
- carte_couleur CHAR NOT NULL CHECK(carte_couleur IN('W', 'B', 'N', 'R', 'V', 'M', 'I')),
+ carte_couleur CHAR NOT NULL CHECK(carte_couleur IN('W', 'B', 'N', 'R', 'V', 'M', 'I', 'T')),
  carte_type VARCHAR(20) NOT NULL CHECK(carte_type IN('terrain', 'creature', 'enchantement', 'rituel', 'ephemere', 'artefact')),
- carte_cout INT NOT NULL,
- carte_force INT CHECK((carte_type = 'creature' AND carte_force >= -1) OR (carte_type != 'creature' AND carte_endurance IS NULL)),
+ carte_cout VARCHAR(10) NOT NULL,
+ carte_force INT CHECK((carte_type = 'creature' AND carte_force >= -1) OR (carte_type != 'creature' AND carte_force IS NULL)),
  carte_endurance INT CHECK((carte_type = 'creature' AND carte_endurance >= -1) OR (carte_type != 'creature' AND carte_endurance IS NULL)),
  carte_artiste VARCHAR(50),
  carte_rarete INT NOT NULL CHECK(carte_rarete BETWEEN 0 AND 4),
  carte_ordre_serie INT NOT NULL,
- ser_code INT NOT NULL ,
+ ser_code VARCHAR(3) NOT NULL ,
  FOREIGN KEY(ser_code) REFERENCES serie(ser_code)
 );
 
 CREATE TABLE carte_langue (
  carte_nom VARCHAR(30) NOT NULL,
- carte_texte VARCHAR(250) NOT NULL,
+ carte_texte VARCHAR(500) NOT NULL,
  carte_id INT NOT NULL,
  lang_id INT NOT NULL,
  FOREIGN KEY(carte_id) REFERENCES carte_virtuelle(carte_id),
