f4972e9c4b560947c15e2babf637e85e134ef02a
Racxys <44971997+Racxys@users.noreply.github.com>
2018-11-27 18:29:56 +0100

    Add files via upload

---
+++ Insertion PGSQL/RemplissageMagic.pgsql.sql
@@ -0,0 +1,14 @@
+Insert INTO serie Values
+('ODY', 'Odyssey', '2001/09/21', 350),
+('TOR', 'Torment', '2002/02/20', 143),
+('JUD', 'Judgment', '2002/05/21', 143);
+
+INSERT INTO langue(lang_nom) Values
+('anglais');
+
+Insert INTO langue_serie VALUES
+(1, 'ODY'),
+(1, 'TOR'),
+(1, 'JUD');
+
+
